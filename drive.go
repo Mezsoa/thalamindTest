a749543aa3dfe2498df2bca62c8c0e76acafc047
John Gunnarsson <johnmessoa@gmail.com>
2025-09-26 16:43:03 +0200

    clear repo history to filter out seecret.

--- drive.go
+++ drive.go
@@ -1,5 +1,20 @@
 package main
 
-func DownloadGoogleDriveFolder() {
+import "os"
 
+
+
+
+
+
+
+
+// DownloadGoogleDriveFolder laddar ner alla filer och undermappar i en Google Drive-mapp rekursivt.
+// anv'nder sem f;r att begr'nnsa antalet samtidiga nedladdningar s[ att inte google API:et blockeras. och kraschar.
+func DownloadGoogleDriveFolder( srv *drive.Service, folderID, localPath string, wg. *sync.WaitGroup, sem chan struct{}) error {
+	
+	// funkar som signa; f;r att denna go routine 'r klar och att funktionen har avslutats
+	defer wg.Done()
+
+	if err := os.MkdirAll()
 }
