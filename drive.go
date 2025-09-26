e72baa20ee5ab51cd652350945d076a4281b300b
John Gunnarsson <johnmessoa@gmail.com>
2025-09-26 18:34:29 +0200

    test projekt done, connecting via OAuth2, fetching FolderID, creates new folder with subFolder structure and copies the response body over local folder and saves it.

--- drive.go
+++ drive.go
@@ -1,20 +1,83 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
 
-
-
-
-
-
-
+	"google.golang.org/api/drive/v3"
+)
 
 // DownloadGoogleDriveFolder laddar ner alla filer och undermappar i en Google Drive-mapp rekursivt.
 // anv'nder sem f;r att begr'nnsa antalet samtidiga nedladdningar s[ att inte google API:et blockeras. och kraschar.
-func DownloadGoogleDriveFolder( srv *drive.Service, folderID, localPath string, wg. *sync.WaitGroup, sem chan struct{}) error {
-	
+func DownloadGoogleDriveFolder(srv *drive.Service, folderID, localPath string, wg *sync.WaitGroup, sem chan struct{}) error {
+
 	// funkar som signa; f;r att denna go routine 'r klar och att funktionen har avslutats
 	defer wg.Done()
 
-	if err := os.MkdirAll()
+	if err := os.MkdirAll(localPath, 0755); err != nil {
+		return fmt.Errorf("kunde inte skapa mapp %s: %v", localPath, err)
+	}
+
+	// listar alla mappar och filer i den drive mapp som jag pekar po
+	query := fmt.Sprintf("'%s' in parents and trashed = false", folderID)
+	fileList, err := srv.Files.List().Q(query).Fields("files(id, name, mimeType)").Do()
+
+	if err != nil {
+		return fmt.Errorf("kunde inte lista filer: %v", err)
+	}
+
+	for _, i := range fileList.Files {
+		if i.MimeType == "application/vnd.google-apps.folder" {
+			// om det 'r en mapp, skapa en ny lokal mapp och kalla p[ funktionen rekursivt
+			subFolderPath := filepath.Join(localPath, i.Name)
+			wg.Add(1)
+			go func() {
+				err := DownloadGoogleDriveFolder(srv, i.Id, subFolderPath, wg, sem)
+				if err != nil {
+
+				}
+			}()
+		} else {
+			// om det 'r en fil, ladda ner den
+			wg.Add(1)
+			go func(file *drive.File) {
+				defer wg.Done()
+				sem <- struct{}{}        // skicka in i sem kanalen f;r att begr'nnsa samtidiga nedladdningar
+				defer func() { <-sem }() // ta bort fr'n sem kanalen n'r nedladdningen 'r klar
+				fmt.Println("nerladdade filer: ", file.Name)
+				if err := downloadFile(srv, file.Id, filepath.Join(localPath, file.Name)); err != nil {
+					fmt.Printf("Fel vid nedladdning av %s: %v\n", file.Name, err)
+				}
+			}(i)
+
+		}
+	}
+	return nil
+}
+
+// downloadFile laddar ner en enskild fil fr'n Google Drive och sparar den lokalt
+func downloadFile(srv *drive.Service, fileID, localPath string) error {
+	// anropar Google Drive API:et f;r att kunna h'mta filen
+	response, err := srv.Files.Get(fileID).Download()
+	// blir det fel, returnera fel
+	if err != nil {
+		return err
+	}
+	// st'nger response n'r vi 'r klara
+	defer response.Body.Close()
+
+	// skapar en lokal fil f;r att spara inneh'lllet
+	outFile, err := os.Create(localPath)
+	if err != nil {
+		return err
+	}
+	// st'nger filen n'r vi 'r klara
+	defer outFile.Close()
+
+	// kopierar response bodyns inneh[ll till den lokala filen outFile som jag skapade ovan
+	_, err = io.Copy(outFile, response.Body)
+	return err
 }
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"log"
 	"os"
+	"path/filepath"
+	"sync"
 
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
@@ -13,25 +15,41 @@ import (
 func main() {
 
 	// läser in credentials.json filen
-	data, err := os.ReadFile("credentials.json")
+	data, err := os.ReadFile("credentialsThalamind.json")
 	if err != nil {
 		log.Fatalf("Kunde inte läsa credentials.json: %v", err)
 	}
-	// Oauth2 config frn credentials filen
+	// skapar en OAuth2 config från credentials filen
 	config, err := google.ConfigFromJSON(data, drive.DriveReadonlyScope)
-
 	if err != nil {
 		log.Fatalf("Kunde inte parsa credentials.json till configen: %v", err)
 	}
 
-	// H'mta en authad http klient
-	client := getClient(config)
+	// H'mta/ får en authad http klient
+	client := GetClient(config)
 
 	// skapar en Drive service fr'n klienten
 	srv, err := drive.NewService(context.Background(), option.WithHTTPClient(client))
-
 	if err != nil {
 		log.Fatalf("Kunde inte skapa Drive service: %v", err)
 	}
 
+	// Go
+	home, _ := os.UserHomeDir()
+
+	// ID fför mappen på google drive som jag vill ladda ner
+	rootFolderID := "1HjVNaFN2G0jIsE8T12CAE94aqE9czNmN"
+	localPath := filepath.Join(home, "Desktop", "GoogleDriveFolder")
+
+	// skapar en waitgroup för att vänta på att alla go routines ska bli klara
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, 5) // sem kanal f;r att begränsa antalet samtidiga nedladdningar s'tter den till 5
+
+	// startar nedladdningen av mappen
+	wg.Add(1)
+	go DownloadGoogleDriveFolder(srv, rootFolderID, localPath, &wg, sem)
+
+	// väntar på att alla nedladdningar ska bli klara
+	wg.Wait()
+	log.Println("Nedladdning klar!")
 }
